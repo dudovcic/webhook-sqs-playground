00d2b2475b593ff49b8f77ed060bed45d42d4137
Denis Udovcic <denisudovcic@gmail.com>
2021-09-13 13:01:54 +0100

    fix missing package + missing return

--- queue-tasks/src/utils/request.ts
+++ queue-tasks/src/utils/request.ts
@@ -4,7 +4,7 @@ export const getRequest = (url: string): Promise<request.ResponseAsJSON> => {
   return new Promise((resolve, reject) => {
     return request.get(url, (err, res) => {
       if (err) {
-        reject(err);
+        return reject(err);
       }
       resolve(res.toJSON());
     });
@@ -20,7 +20,7 @@ export const postRequest = (
       if (err) {
         return reject(err);
       }
-      resolve(res.body);
+      resolve(res.toJSON());
     });
   });
 };
